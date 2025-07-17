775f37b3c63403c1c851485613c8d4423bbf3729
Vedansh Srivastava <72293565+vedanshji@users.noreply.github.com>
2025-07-17 18:20:48 +0530

    Update skills.tsx

--- components/main/skills.tsx
+++ components/main/skills.tsx
@@ -59,4 +59,44 @@ export const Skills = () => {
         ))}
       </div>
 
-      <div className="flex flex-row justify-around flex
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {backendSkills.map((skill, i) => (
+          <SkillDataProvider
+            key={skill.skill_name}
+            src={skill.image}
+            name={skill.skill_name}
+            width={skill.width}
+            height={skill.height}
+            index={i}
+          />
+        ))}
+      </div>
+
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {fullstackSkills.map((skill, i) => (
+          <SkillDataProvider
+            key={skill.skill_name}
+            src={skill.image}
+            name={skill.skill_name}
+            width={skill.width}
+            height={skill.height}
+            index={i}
+          />
+        ))}
+      </div>
+
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {otherSkills.map((skill, i) => (
+          <SkillDataProvider
+            key={skill.skill_name}
+            src={skill.image}
+            name={skill.skill_name}
+            width={skill.width}
+            height={skill.height}
+            index={i}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
